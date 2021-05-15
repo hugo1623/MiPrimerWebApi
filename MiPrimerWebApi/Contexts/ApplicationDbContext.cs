136fb631f212683a32047f6a65d0133ef1df0dc6
Hugo Luis <hugoluis.system@gmail.com>
2021-05-15 09:54:37 -0500

    Creacion de DB y tabla Autor

---
+++ MiPrimerWebApi/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApi.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
