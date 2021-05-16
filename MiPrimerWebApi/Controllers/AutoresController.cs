6ee3e925a375f935e43bb45a99a89b2a034f7ea8
Hugo Luis <hugoluis.system@gmail.com>
2021-05-16 18:36:59 -0500

    Creación de recursos desde el controlador

---
+++ MiPrimerWebApi/Controllers/AutoresController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiPrimerWebApi.Contexts;
+using MiPrimerWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+        [HttpGet("{id}", Name = "ObtenerAutor")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(x => x.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            context.Autores.Add(autor);
+            context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObtenerAutor", new { Id = autor.Id }, autor);
+        }
+    }
+}
