389416dba44c9bf9abb4519145fdcb2f0fe4b1af
hugo1623 <hugoluis.system@gmail.com>
2021-06-19 07:05:17 -0500

    Reglas de Validacion

--- MiPrimerWebApi/Entities/Autor.cs
+++ MiPrimerWebApi/Entities/Autor.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace MiPrimerWebApi.Entities
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="EL campo nombre es requerido")]
         [StringLength(maximumLength: 4, ErrorMessage ="El campo {0} no debe tener más de {1} carácteres")]
-        [PrimeraLetraMayuscula]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         [Range(18,120)] 
         [NotMapped]
@@ -25,5 +25,25 @@ namespace MiPrimerWebApi.Entities
         [NotMapped]
         public string  URL { get; set; }
         public List<Libro> Libros { get; set; }
+
+        public int Menor { get; set; }
+        public int Mayor { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra != Nombre[0].ToString())
+                {
+                    yield return new ValidationResult("La primera Letra debe ser Mayuscula",
+                        new string[] {nameof(Nombre)});
+                }
+            }
+            if(Menor>Mayor)
+            {
+                yield return new ValidationResult("Este valor no puede ser más grande que el campo Mayor",
+                    new string[] { nameof(Menor)});
+            }
+        }
     }
 }
